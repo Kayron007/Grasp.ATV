c912d88d4e68117ddccd9949900b65c151662492
I'm Kayron <kayron.gabriel2014@gmail.com>
2025-11-11 06:56:19 +0000

    Adicionando arquivo main

---
+++ Main.java
@@ -0,0 +1,14 @@
+package br.ucb.grasp;
+
+import br.ucb.grasp.controller.OrderController;
+
+/**
+ * Main – ponto de entrada da aplicação.
+ * Aplica GRASP (Controller): delega o caso de uso "realizar pedido" ao OrderController.
+ */
+public class Main {
+    public static void main(String[] args) {
+        OrderController controller = new OrderController();
+        controller.demo();
+    }
+}
