85ea3dcb9cf1b2b4a5abb84ed1d101054a97437a
I'm Kayron <kayron.gabriel2014@gmail.com>
2025-11-11 07:15:49 +0000

    Atualiza lógica e implementação em OrderController, OrderItem e Customer

---
+++ Customer.java
@@ -0,0 +1,7 @@
+package br.ucb.grasp.domain;
+
+/**
+ * Alta coesão e baixo acoplamento:
+ * Representa o cliente sem depender de outras camadas.
+ */
+public record Customer(Long id, String name) { }
