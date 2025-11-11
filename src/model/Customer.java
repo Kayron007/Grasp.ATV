77456e3ba640c9b8507d62586b113b2dc51e4657
Kayron Gabriel Gomes Nascimento <kayron.gabriel2014@gmail.com>
2025-11-11 19:51:39 -0300

    Removendo formato antigo e adaptando para mvc model

--- src/model/Customer.java
+++
@@ -1,8 +0,0 @@
-package br.ucb.grasp.domain;
-
-/**
- * Alta coesão e baixo acoplamento:
- * Representa o cliente sem depender de outras camadas.
- */
-public record Customer(Long id, String name) { }
-//Organização
